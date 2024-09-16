576503ea18c0e29e2317cf780e81ac6882254058
teha <tehaabdurahman@gmail.com>
2024-09-16 08:29:50 -0700

    modidfy vite cogige

--- netflix-cloning-project/vite.config.js
+++ netflix-cloning-project/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/Netflex-clone/',
-})
+  base: "/Netflex-clone/",
+  plugins: [react()],
+});
