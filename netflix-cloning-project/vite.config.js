1afe82e8d62afdd1fa15f372d7d806f0da23c518
teha <tehaabdurahman@gmail.com>
2024-11-23 23:39:42 -0800

    update

--- netflix-cloning-project/vite.config.js
+++ netflix-cloning-project/vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Vite configuration
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["axios"], // Marks Axios as external (if you’re using a CDN or similar external source)
-    },
-  },
 });
